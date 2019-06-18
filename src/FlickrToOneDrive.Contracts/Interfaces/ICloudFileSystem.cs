4fb8c5e6d60843f6532ba5d0859174297b370228
havlicekp <havlicekp@gmail.com>
2019-06-18 20:41:39 +0200

    Added Async to async method names

--- src/FlickrToOneDrive.Contracts/Interfaces/ICloudFileSystem.cs
+++ src/FlickrToOneDrive.Contracts/Interfaces/ICloudFileSystem.cs
@@ -11,12 +11,13 @@ namespace FlickrToOneDrive.Contracts.Interfaces
         Task<string> GetAuthenticationUrl();
         string Name { get; }
         bool IsAuthenticated { get; }
-        Task<string> UploadFileFromUrl(string path, File file);
-        Task UploadFile(string destinationFilePath, string localFileName, CancellationToken ct);
-        Task<OperationStatus> CheckOperationStatus(File file);
-        Task<File[]> GetFiles(SessionFilesOrigin filesOrigin, CancellationToken ct, Action<ReadingFilesProgress> progressHandler = null);
-        Task<bool> FolderExists(string folder);
-        Task<bool> CreateFolder(string folder);
-        Task<string[]> GetSubFolders(string folder);
+        Task<string> UploadFileFromUrlAsync(string path, File file, CancellationToken ct);
+        Task UploadFileAsync(string destinationFilePath, string localFileName, CancellationToken ct);
+        Task<OperationStatus> CheckOperationStatusAsync(File file, CancellationToken ct);
+        Task<File[]> GetFilesAsync(SessionFilesOrigin filesOrigin, CancellationToken ct, Action<ReadingFilesProgress> progressHandler = null);
+        Task<bool> FolderExistsAsync(string folder, CancellationToken ct);
+        Task<bool> CreateFolderAsync(string folder, CancellationToken ct);
+        Task<string[]> GetSubFoldersAsync(string folder, CancellationToken ct);
+        Task CopyFileAsync(string fromFilePath, string toPath, CancellationToken ct);
     }
 }
